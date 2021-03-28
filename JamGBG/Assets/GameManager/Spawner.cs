7febcadfe73c2b0f47ce6d557baf5606ba291d82
douglasdriving <douglasdriving@gmail.com>
2021-03-28 16:39:46 +0200

    added some sprites

--- JamGBG/Assets/GameManager/Spawner.cs
+++ JamGBG/Assets/GameManager/Spawner.cs
@@ -11,6 +11,9 @@ public class Spawner : MonoBehaviour
 	GameObject scoreCounterText;
 	int numberOfPlayers;
 
+	[SerializeField] Sprite[] playerSprites;
+	[SerializeField] Sprite[] platformSprites;
+
 	public IEnumerator PlaceObjects()
 	{
 		winZone = GameManager.Instance.winZone;
@@ -33,7 +36,7 @@ public class Spawner : MonoBehaviour
 		for (int i = 0; i < numberOfPlayers; i++)
 		{
 			// Instantiate(winZone, new Vector3(widthMin + shortDistance + longDistance * i, heightMin + (heightMax * 0.75f), 0), Quaternion.identity).GetComponent<WinZoneOwner>().playerID = "Player " + (i + 1).ToString();
-			GameObject zone = Instantiate(winZone, new Vector3(widthMin + shortDistance + longDistance * i, heightMin + (heightMax * 0.03f), 0), Quaternion.identity);
+			GameObject zone = Instantiate(winZone, new Vector3(widthMin + shortDistance + longDistance * i, heightMin + 3, 0), Quaternion.identity);
 			zone.transform.SetParent(null);
 
 			Vector3 zonePos = Camera.main.WorldToScreenPoint(zone.transform.position);
@@ -41,24 +44,33 @@ public class Spawner : MonoBehaviour
 			GameObject tempText = Instantiate(scoreCounterText, new Vector3(zonePos.x - 120, zonePos.y + 50, 0), Quaternion.identity);
 			tempText.transform.SetParent(FindObjectOfType<Canvas>().transform);
 			zone.GetComponent<ZoneBrain>().scoreTextField = tempText;
+
+			//SET GROUND SPRITE
+			foreach (Transform child in zone.transform)
+            {
+				if (child.CompareTag("Ground"))
+                {
+					child.GetComponent<SpriteRenderer>().sprite = platformSprites[i];
+                }
+            }
 		}
 
 		for (int i = 0; i < numberOfPlayers; i++)
 		{
+			GameObject newPlayer;
 			if (i == 0)
 			{
 				Destroy(Instantiate(player, new Vector3(10000, heightMin + (heightMax * 0.8f), 0), Quaternion.identity), 0.01f);
-				Instantiate(player, new Vector3(widthMin + shortDistance + longDistance * i, heightMin + (heightMax * 0.8f), 0), Quaternion.identity);
+				newPlayer = Instantiate(player, new Vector3(widthMin + shortDistance + longDistance * i, heightMin + heightMax - 2.15f, 0), Quaternion.identity);
 			}
 			else
 			{
 				Debug.Log("Player joined");
-				GameObject temp = Instantiate(player, new Vector3(widthMin + shortDistance + longDistance * i, heightMin + (heightMax * 0.8f), 0), Quaternion.identity);
-				temp.GetComponent<PlayerInput>().SwitchCurrentActionMap("Gamer" + i.ToString());
-				temp.transform.SetParent(null);
+				newPlayer = Instantiate(player, new Vector3(widthMin + shortDistance + longDistance * i, heightMin + heightMax - 2.15f, 0), Quaternion.identity);
+				newPlayer.GetComponent<PlayerInput>().SwitchCurrentActionMap("Gamer" + i.ToString());
+				newPlayer.transform.SetParent(null);
 			}
-
-
+			newPlayer.GetComponent<SpriteRenderer>().sprite = playerSprites[i];
 		}
 	}
 }
